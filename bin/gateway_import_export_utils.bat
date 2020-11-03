99fb1db4dea867a44752acbcceecb4ff92c4f354
rakeshadupa <rakeshadupa931@gmail.com>
2020-11-03 15:00:29 +0530

    f

--- bin/gateway_import_export_utils.bat
+++ bin/gateway_import_export_utils.bat
@@ -4,22 +4,31 @@ set CURRENT_DIR=%~dp0
 
 set ACTION=%1
 SHIFT 
+def 
 
 :loop
 IF NOT "%1"=="" (
+SET PROJECT_NAME=sample
+SET GATEWAY_URL=http://localhost:5566
+
+echo first if===
     IF "%1"=="_api_name" (
+echo second if===
         SET PROJECT_NAME=%3
         SHIFT
     )
     IF "%1"=="-apigateway_url" (
+echo third if===
         SET GATEWAY_URL=%2
         SHIFT
     )
 	IF "%1"=="-apigateway_username" (
+echo 4th if===
         SET GATEWAY_USERNAME=%2
         SHIFT
     )
 	IF "%1"=="-apigateway_password" (
+echo 5th if===
         SET GATEWAY_PASSWORD=%2
         SHIFT
     )
@@ -35,7 +44,7 @@ goto import
 
 :import 
 IF EXIST %CURRENT_DIR%/apis/%PROJECT_NAME%/ (
- powershell Compress-Archive -Path %CURRENT_DIR%/apis/%PROJECT_NAME%/* -DestinationPath %CURRENT_DIR%/%PROJECT_NAME%.zip -Force
+ powershell Compress-Archive -Path %CURRENT_DIR%/apis/%PROJECT_NAME%/* -DestinationPath %CURRENT_DIR%\%PROJECT_NAME%.zip -Force
  curl -i -X POST %GATEWAY_URL%/rest/apigateway/archive?overwrite=* -H "Content-Type: application/zip" -H "Accept:application/json" --data-binary @"%CURRENT_DIR%/%PROJECT_NAME%.zip" --user %GATEWAY_USERNAME%:%GATEWAY_PASSWORD%
  del "%CURRENT_DIR%/%PROJECT_NAME%.zip"
  goto :EOF
@@ -46,19 +55,18 @@ IF EXIST %CURRENT_DIR%/apis/%PROJECT_NAME%/ (
 
 :export
 IF EXIST %CURRENT_DIR%/apis/%PROJECT_NAME%/ (
- curl %GATEWAY_URL%/rest/apigateway/archive -d @"%CURRENT_DIR%\apis\%PROJECT_NAME%\export_payload.json" --output %CURRENT_DIR%/%PROJECT_NAME%.zip -u  Administrator:manage -H "x-HTTP-Method-Override: GET" -H "Content-Type:application/json"
+ curl %GATEWAY_URL%/rest/apigateway/archive -d @"%CURRENT_DIR%\apis\%PROJECT_NAME%\export_payload.json" --output %CURRENT_DIR%/%PROJECT_NAME%.zip --user  Administrator:manage -H "x-HTTP-Method-Override: GET" -H "Content-Type:application/json"
  powershell Expand-Archive -Path %CURRENT_DIR%/%PROJECT_NAME%.zip -DestinationPath %CURRENT_DIR%/apis/%PROJECT_NAME% -Force
  del "%CURRENT_DIR%/%PROJECT_NAME%.zip"
 goto :EOF
 ) ELSE (
-  echo "API with name %PROJECT_NAME% does not exists"
+  echo "API with name %PROJECT_NAME% does not exists in export--"
   goto :EOF
 )
 
 
 
  
-
 
 
 
